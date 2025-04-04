ce086b3358fa2d61cd0076085370028cd4782d7d
admin <bosko15082004@gmail.com>
2025-04-04 09:30:12 +0300

    have done an api-gateway realisation

---
+++ api_gateway_service/src/main/kotlin/chat/api_gateway_service/ApiGatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package chat.api_gateway_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGatewayServiceApplication>(*args)
+}
